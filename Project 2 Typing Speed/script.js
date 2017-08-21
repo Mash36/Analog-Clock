a7f24c56179d5938680ed3aacc1bc3ca9604f886
Mash <z5102072@student.unsw.edu.au>
2017-08-21 20:52:16 +1000

    Project 2 working

--- Project 2 Typing Speed/script.js
+++ Project 2 Typing Speed/script.js
@@ -4,20 +4,75 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = [0,0,0,0];
+var interval;
+var timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time) {
+    if (time <= 9) time = "0" + time;
+    return time;
+}
 
 // Run a standard minute/second/hundredths timer:
+function runTimer() {
+    let currTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    theTimer.innerHTML = currTime;
+    timer[3]++;
 
+    timer[0] = Math.floor((timer[3]/100)/60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
 
 // Match the text entered with the provided text on the page:
+function spellCheck() {
+    let textEntered = testArea.value;
+    let originTextMatch = originText.substring(0,textEntered.length);
 
+    if (textEntered == originText) {
+        clearInterval(interval);
+        testWrapper.style.borderColor = "green";
+    } else if (textEntered == originTextMatch){
+        testWrapper.style.borderColor = "blue";
+    } else {
+        testWrapper.style.borderColor = "red";
+    }
+    console.log(textEntered);
+}
 
 // Start the timer:
+function start() {
+    let textLength = testArea.value.length;
+    if (textLength === 0 && !timerRunning) {
+        timerRunning = true;
+        interval = setInterval(runTimer, 10);
+    }
 
+    console.log(textLength);
+}
 
 // Reset everything:
+function reset() {
+    clearInterval(interval);
+    interval = null;
+    timer = [0,0,0,0];
+    timerRunning = false;
 
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
+}
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keypress",start,false);
+testArea.addEventListener("keyup", spellCheck,false);
+resetButton.addEventListener("click",reset,false);
+
+// Set Interval
+// The setInterval() method of the WindowOrWorkerGlobalScope
+// mixin repeatedly calls a function or executes a code snippet,
+// with a fixed time delay between each call. It returns an
+// interval ID which uniquely identifies the interval, so you
+// can remove it later by calling clearInterval(). This method
+// is offered on the Window and Worker interfaces.
